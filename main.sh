70655ac5820dc87bc6ff8540ade52ec6a156d891
Artur Dandolini Pescador <artur.pescador@hotmail.com>
2023-12-29 14:55:06 +0100

    update main.sh with quality assesment

--- main.sh
+++ main.sh
@@ -1,5 +1,7 @@
-( python ./energy-weather-database/generate_iea.py -f 2023-12-20 -t 2023-12-25 -ts 2; \
-  python ./energy-weather-database/generate_weather.py -f 2023-12-20 -t 2023-12-25 -ts 2 ) | \
-python ./energy-weather-database/mapreduce/database_merger/mapper.py | \
-python ./energy-weather-database/mapreduce/database_merger/shuffler.py | \
-python ./energy-weather-database/mapreduce/database_merger/reducer.py
+( python ./generate_iea.py -f 2023-12-20 -t 2023-12-25 -ts 2; \
+  python ./generate_weather.py -f 2023-12-20 -t 2023-12-25 -ts 2 ) | \
+python ./mapreduce/database_merger/mapper.py | \
+python ./mapreduce/database_merger/shuffler.py | \
+python ./mapreduce/database_merger/reducer.py > merged_data.csv
+
+python ./quality_assessment.py merged_data.csv
