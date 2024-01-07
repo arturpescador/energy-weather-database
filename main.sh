e6d28594c0988a207f541899170433bf49d3f99c
Artur Dandolini Pescador <artur.pescador@hotmail.com>
2024-01-07 12:03:25 +0100

    centralizing all into main.sh

--- main.sh
+++ main.sh
@@ -1,6 +1,24 @@
+# Generate and Merge Data
 ( python ./generate_iea.py -f 2023-12-20 -t 2023-12-25 -ts 2; \
   python ./generate_weather.py -f 2023-12-20 -t 2023-12-25 -ts 2 ) | \
 python ./mapreduce/database_merger/mapper.py | \
 python ./mapreduce/database_merger/shuffler.py | \
 python ./mapreduce/database_merger/reducer.py > merged_data.csv
+
+# Quality Assessment
 python ./quality_assessment.py merged_data.csv
+
+# Create New Folder and Move File
+mkdir -p dataset
+mv merged_data.csv dataset/
+
+# Scrape Weather Data
+cd weather_news
+scrapy crawl weather_spd
+python process_data.py -i weather_data.json -o weather_data.csv
+
+# Move the Weather Data CSV
+mv weather_data.csv ../dataset/
+
+# Return to the original directory
+cd ..
